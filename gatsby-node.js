35f4321b6084650c20686f8f85cd537fb1404a02
rogermparent <rogermparent@gmail.com>
2020-05-21 11:58:10 -0400

    Configure webpack to provide a dummy fs at runtime
    Something we have imports fs at runtime despite not using it, but this small
    Webpack config change solves that problem for all modules in the future.
    
    It's worth noting other MDX projects don't require this, so we should
    investigate why we need this sometime. I tried, but the stack traces aren't too
    friendly...

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,9 +22,14 @@ exports.onCreatePage = ({ page, actions }) => {
 // Ignore warnings about CSS inclusion order, because we use CSS modules.
 // https://spectrum.chat/gatsby-js/general/having-issue-related-to-chunk-commons-mini-css-extract-plugin~0ee9c456-a37e-472a-a1a0-cc36f8ae6033?m=MTU3MjYyNDQ5OTAyNQ==
 exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
-  if (stage === 'build-javascript') {
-    const config = getConfig()
+  const config = getConfig()
 
+  config.node = {
+    ...(config.node || {}),
+    fs: 'empty'
+  }
+
+  if (stage === 'build-javascript') {
     // Add polyfills
     config.entry.app = [
       'promise-polyfill/src/polyfill',
@@ -39,8 +44,9 @@ exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
     if (miniCssExtractPlugin) {
       miniCssExtractPlugin.options.ignoreOrder = true
     }
-    actions.replaceWebpackConfig(config)
   }
+
+  actions.replaceWebpackConfig(config)
 }
 
 exports.onPostBuild = api => {
